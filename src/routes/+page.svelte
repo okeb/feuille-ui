2ac6acbf8f453648eea47c99dfa5f225738769a8
Oliver KEB <oliver.keb@icloud.com>
2023-01-20 23:29:07 +0100

    📃 docs(og): update site infos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Bienvenu sur Feuille UI</h1>
+<img src="favicon-32x32.png" alt="logo de feuille ui">
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
