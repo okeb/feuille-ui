cdb0c3dea9234362dab0e5b3ae5a55ab6fd23a36
Oliver KEB <oliver.keb@icloud.com>
2023-01-21 00:36:39 +0100

    🐞 fix(preview):
    try ti fix again the link preview image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1>Bienvenu sur Feuille UI</h1>
-<img src="favicon-32x32.png" alt="logo de feuille ui">
+<img src="thumbnail.png" alt="logo de feuille ui">
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
