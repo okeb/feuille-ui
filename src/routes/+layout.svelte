7658f10f83a606eef2b4fa02fe142e1736a922ed
Oliver KEB <oliver.keb@icloud.com>
2023-01-23 00:41:42 +0100

    ✨ feat(themetoggle):
    add button to toggle the theme mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,54 @@
 <script lang="ts">
+  // import type { onMount } from "svelte";
   import "virtual:windi.css"
+
+  let mode:boolean = true
+
+  const toggleMode = () => {
+    if (localStorage.getItem('color-theme')) {
+      if (localStorage.getItem('color-theme') === 'light') {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('color-theme', 'dark');
+      } else {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('color-theme', 'light');
+      }
+    } else {
+      if (document.documentElement.classList.contains('dark')) {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('color-theme', 'light');
+      } else {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('color-theme', 'dark');
+      }
+    }
+    mode = !mode
+    console.log(mode)
+  }
+
+
 </script>
 
-<nav class="border-b border-b-[#333333] py-4 px-10">
-  <a href="/">Home</a>
-  <a href="/posts">Posts</a>
+<nav class="border-b border-b-[#EBEBEB] dark:border-b-[#333333] py-3 px-12 grid grid-cols-2 transition-colors">
+  <section>
+    <img src="favicon-96.png" alt="logo de feuille UI" class="h-10 p-0">
+  </section>
+  <section class="flex justify-end content-center gap-8">
+    <a href="/" class="a_navItem">Get started</a>
+    <a href="/" class="a_navItem">Documentation API</a>
+    <a href="/" class="a_navItem">Connexion</a>
+    <a href="/" class="a_navItem">
+      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path fill="currentColor" d="M12 .297c-6.63 0-12 5.373-12 12c0 5.303 3.438 9.8 8.205 11.385c.6.113.82-.258.82-.577c0-.285-.01-1.04-.015-2.04c-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729c1.205.084 1.838 1.236 1.838 1.236c1.07 1.835 2.809 1.305 3.495.998c.108-.776.417-1.305.76-1.605c-2.665-.3-5.466-1.332-5.466-5.93c0-1.31.465-2.38 1.235-3.22c-.135-.303-.54-1.523.105-3.176c0 0 1.005-.322 3.3 1.23c.96-.267 1.98-.399 3-.405c1.02.006 2.04.138 3 .405c2.28-1.552 3.285-1.23 3.285-1.23c.645 1.653.24 2.873.12 3.176c.765.84 1.23 1.91 1.23 3.22c0 4.61-2.805 5.625-5.475 5.92c.42.36.81 1.096.81 2.22c0 1.606-.015 2.896-.015 3.286c0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
+    </a>
+
+    <div class="a_navItem shadow-green-500/50 shadow-xl" on:click={toggleMode} on:keypress={() => {console.log(mode)}}>
+      {#if mode}
+          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path fill="currentColor" d="M13 0h-2v4h2V0ZM0 11v2h4v-2H0Zm24 0v2h-4v-2h4ZM13 24h-2v-4h2v4ZM8 6h8v2H8V6ZM6 8h2v8H6V8Zm2 10v-2h8v2H8Zm10-2h-2V8h2v8Zm2-14h2v2h-2V2Zm0 2v2h-2V4h2Zm2 18h-2v-2h2v2Zm-2-2h-2v-2h2v2ZM4 2H2v2h2v2h2V4H4V2ZM2 22h2v-2h2v-2H4v2H2v2Z"/></svg>
+      {:else}
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path fill="currentColor" d="M6 2h8v2h-2v2h-2V4H6V2ZM4 6V4h2v2H4Zm0 10H2V6h2v10Zm2 2H4v-2h2v2Zm2 2H6v-2h2v2Zm10 0v2H8v-2h10Zm2-2v2h-2v-2h2Zm-2-4v-2h2v-2h2v8h-2v-4h-2Zm-6 0h6v2h-6v-2Zm-2-2h2v2h-2v-2Zm0 0V6H8v6h2Zm8-10h2v2h2v2h-2v2h-2V6h-2V4h2V2Z"/></svg>
+      {/if}
+    </div>
+
+  </section>
 </nav>
 <slot/>
--- windi.config.ts
+++ windi.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'windicss/helpers'
 // import formsPlugin from 'windicss/plugin/forms'
 
 export default defineConfig({
-  darkMode: 'media',
+  darkMode: 'class',
   extract: {
     include: ['src/**/*.{svelte,html,jsx,tsx}'],
     exclude: ['node_modules', '.git'],
@@ -11,15 +11,26 @@ export default defineConfig({
   theme: {
     extend: {
       fontFamily: {
-        body: ['Open Sans']
-      }
-      // colors: {
-      //   teal: {
-      //     100: '#096',
-      //   },
+        body: ['Unbounded', 'ui-sans-serif', 'system-ui'],
+        over: ['Overpass', 'sans-serif'],
+        unbounded: ['Unbounded', 'cursive'],
+        sans: ['Work Sans', 'sans-serif', 'ui-sans-serif', 'system-ui']
+      },
+      colors: {
+        night: '#777777',
+        teal: {
+          100: '#096',
+        },
       },
     },
   },
-  // plugins: [formsPlugin],
-// }
-)
+  shortcuts: {
+    'a_navItem': {
+      '@apply': 'p-0 my-auto align-baseline text-[14px] transition-all text-[#222222] dark:text-white',
+      '&:hover': {
+        '@apply': 'transition-all text-[#555555] dark:text-[#777777]',
+      }
+    },
+    'btn-green': 'text-white bg-green-500 hover:bg-green-700',
+  },
+})
